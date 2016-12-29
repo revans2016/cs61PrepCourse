e1879202c5ccd5407533aba8e857cf872541083d
revans2016 <revans2016@berkeley.edu>
2016-12-28 18:04:01 -0800

    Add files via upload

---
+++ ch6/angryboss.rb
@@ -0,0 +1,4 @@
+#angry boss code
+puts 'Alright what do you want?'
+request = gets.chomp.to_s
+puts 'WHADDAYA MEAN "' + request + '"?!? YOU\'RE FIRED!!' 
---
+++ ch6/tablecontents.rb
@@ -0,0 +1,6 @@
+#Table of Contents 
+line_width = 40 
+puts ('Table of Contents'.center(line_width))
+puts ('Chapter 1: Getting Started'.ljust(line_width/2) + 'page 1'.rjust(14))
+puts ('Chapter 2: Numbers'.ljust(line_width/2) + 'page 9'.rjust(line_width/2))
+puts ('Chapter 3: Letters'.ljust(line_width/2) + 'page 13'.rjust(21))
