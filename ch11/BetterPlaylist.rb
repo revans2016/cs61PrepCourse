f9af714bfd09d1cb0809c3bd0e4942613d0de002
revans2016 <revans2016@berkeley.edu>
2017-01-11 21:52:50 -0800

    Add files via upload

--- ch11/BetterPlaylist.rb
+++ ch11/BetterPlaylist.rb
@@ -1,15 +1,37 @@
-# my music_shuffle is supposed to take the last file off of the original array,
-# add it to a new array to be the first index,
-# then add the remaining old array to the new array
-def music_shuffle filenames
-	filenames = filenames.sort
-	len = filenames.length
-	arr = []
-	while true 
-		thing = filenames.pop
-		arr.push(thing)
-		arr.push(filenames)
+# dive array into two different arrays
+# shuffle the two array individually
+# join two new arrays
+def shuffle arr
+	shuf = []
+	while arr.length>0
+		rand_index = rand(arr.length)
+		curr_index = 0
+		new_arr = []
+		arr.each do |item|
+			if curr_index == rand_index
+				shuf.push item
+			else 
+				new_arr.push item
+			end
+			curr_index = curr_index + 1
+		end
+		arr = new_arr
 	end
+	shuf
+end
+
+
+def music_shuffle filenames
+	len = filenames.length
+	new_array = []
+	2.times do 
+		thing = filenames.pop
+		new_array.push(thing)
+	end
+	shuffle(filenames)
+	shuffle(new_array)
+	final_array = filenames + new_array
+	filenames = final_array
 end
 
 songz = ['rr/eee', 'AAA/bbb', 'dog/cat', 'penguin/land']
